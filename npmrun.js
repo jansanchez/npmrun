48c5c41b3b6f0f27265709ab12c5b713cb577890
jansanchez <joejansanchez@gmail.com>
2016-01-27 15:46:42 -0500

    feat(package): actualizacion de tarea

--- npmrun.js
+++ npmrun.js
@@ -4,11 +4,15 @@ var program = require('commander');
 
 program
     .version(require('./package.json').version)
-    .command('nr task_name')
-    .action(function (task) {
-        console.log("entro");
+    .command('nr <task_name> [tasks...]')
+    .action(function (task, otherTasks) {
         console.log('nr %s', task); 
+        if (otherTasks){
+            otherTasks.forEach(function (oTasks){
+            })
+        }
     });
 
 program.parse(process.argv);
 
+// console.log("hola amigos");
