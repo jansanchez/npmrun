cc6270ead2c08ea8fa3bb2e523f0c52119162df5
jansanchez <joejansanchez@gmail.com>
2016-01-28 14:48:20 -0500

    chore(nr): agrego funcionalidad de consola

--- npmrun.js
+++ npmrun.js
@@ -1,19 +1,28 @@
-#!/usr/bin/env node
+
 
 var program = require('commander');
+var exec = require('child_process').exec;
+var color = require('chalk');
 
-program
-    .version(require('./package.json').version)
-    .command('nr <task_name> [tasks...]')
-    .action(function (task, otherTasks) {
-        console.log('nr %s', task); 
-        if (otherTasks){
-            otherTasks.forEach(function (oTask){
-                console.log('nr %s', oTask);
-            });
+var execute = function (task){
+    exec("npm run " + task, function (error, stdout, stderr) {
+        console.log(color.magenta(stdout));
+        if (error !== null) {
+            // console.log(color.red('exec error: ' + error));
         }
     });
+}
+
+program
+.version(require('./package.json').version)
+.command('nr <task_name> [tasks...]')
+.action(function (task, otherTasks) {
+    execute(task);         
+    if (otherTasks){
+        otherTasks.forEach(function (oTask){
+            execute(task);         
+        });
+    }
+});
 
 program.parse(process.argv);
-
-// console.log("hola amigos");
