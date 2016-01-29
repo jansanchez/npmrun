6f659df9bcb7a7322c5d0684e7801b794eced844
Jan Sanchez <joejansanchez@gmail.com>
2016-01-28 21:40:33 -0500

    fix(nr): corrijo error en consola

--- npmrun.js
+++ npmrun.js
@@ -5,24 +5,22 @@ var exec = require('child_process').exec;
 var color = require('chalk');
 
 var execute = function (task){
-    exec("npm run " + task, function (error, stdout, stderr) {
-        console.log(color.magenta(stdout));
-        if (error !== null) {
-            // console.log(color.red('exec error: ' + error));
-        }
-    });
+	exec("npm run " + task, function (error, stdout, stderr) {
+		console.log(color.magenta(stdout));
+	});
 }
 
 program
 .version(require('./package.json').version)
-.command('nr <task_name> [tasks...]')
 .action(function (task, otherTasks) {
-    execute(task);         
-    if (otherTasks){
-        otherTasks.forEach(function (oTask){
-            execute(task);         
-        });
-    }
+	execute(task);
+	// console.log(otherTasks);
+	// if (otherTasks){
+	//     otherTasks.forEach(function (oTask){
+	//         execute(task);
+	// 	});
+	// }
 });
 
 program.parse(process.argv);
+
