7b81f54e8db44118d4ecba7246d8092919cd967f
Jan Sanchez <joejansanchez@gmail.com>
2016-01-31 10:08:17 -0500

    feat(es6): update to es6

--- npmrun.js
+++ npmrun.js
@@ -1,27 +1,38 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var exec = require('child_process').exec;
-var color = require('chalk');
+'use strict';
 
-var execute = function (task){
-	exec("npm run " + task, function (error, stdout, stderr) {
+const program = require('commander');
+const exec = require('child_process').exec;
+const color = require('chalk');
+const execute = function (task) {
+	exec(`npm run ${task}`, (error, stdout, stderr) => {
 		console.log(color.magenta(stdout));
-		console.log(color.gray(stderr));
+		console.log(color.red.bold(stderr));
+		console.log(color.white.underline(Array(26).join('  ')));
+		console.log(Array(26).join('  '));
 	});
-}
+};
+
+let tasks = false;
 
 program
 .version(require('./package.json').version)
-.action(function (task, otherTasks) {
+.arguments('<task> [otherTasks...]')
+.action((task, otherTasks) => {
+	tasks = true;
 	execute(task);
-	// console.log(otherTasks);
-	// if (otherTasks){
-	//     otherTasks.forEach(function (oTask){
-	//         execute(task);
-	// 	});
-	// }
+	if (otherTasks.length !== 0) {
+		otherTasks.forEach(oTask => {
+			execute(oTask);
+		});
+	}
 });
 
 program.parse(process.argv);
+
+if (tasks === false) {
+	console.error(color.red.bold('no command given!'));
+	process.exit(1);
+}
 
